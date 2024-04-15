5fbc8d329abd99af79d358f16bbf409ebfc5375f
FatKa1707 <163551346+FatKa1707@users.noreply.github.com>
2024-04-16 00:19:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ $(document).ready(function(){
     //Tìm tất cả các li có sub-menu và thêm vào nó class has-child
     $('.sub-menu').parent('li').addClass('has-child');
 });
+
+//Cuộn trang giữ menu 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $("header").addClass('sticky');
+        }
+        else{
+            $("header").removeClass('sticky');
+        }
+    });
+});
