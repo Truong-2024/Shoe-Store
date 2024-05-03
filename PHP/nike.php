6fd163a4cb493456e134f98ff16cde58884df6ad
FatKa1707 <163551346+FatKa1707@users.noreply.github.com>
2024-05-03 21:11:43 +0700

    Update nike.php

--- PHP/nike.php
+++ PHP/nike.php
@@ -5,156 +5,188 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/sanpham.css">
     
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> <!-- Thêm Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
-    <nav class="navbar">
-        <div class="logo">
-            <a href="#">TRUONGPHAT</a>
-        </div>
-        <div class="right-menu">
-            <ul class="menu">
-                <li><a href="adidas.html">ADIDAS</a></li>
-                <li><a href="nike.html">NIKE</a></li>
-                <li><a href="giaytay.html">GIÀY TÂY</a></li>
-                <li><a href="dep.html">DÉP</a></li>
-                <li><a href="#">GIỚI THIỆU</a></li>
-            </ul>
-            <div class="search">
-                <input type="text" placeholder="Tìm kiếm...">
-                <button type="submit"><i class="fas fa-search"></i></button>
-            </div>
-            <div class="account">
-                <a href="#"><i class="fas fa-user"></i></a>
-                <div class="account-info">
-                    <!-- Thông tin đăng nhập và đăng ký sẽ hiển thị ở đây -->
+    <div class="wapper">
+        <header>
+            <nav class="container">
+                <a href="" id="logo">
+                    <img src="../Image/LOGO/Remove-bg.ai_1711328789070.png" alt="">
+                </a>
+
+                <ul id="main-menu">
+                    <li><a href="">Trang chủ</a></li>
+
+                    <li><a href="">Sản phẩm</a>
+                        <ul class="sub-menu">
+                            <li><a href="">Menu 2.1</a>
+                                <ul class="sub-menu">
+                                    <li><a href="">Menu 2.2.1</a></li>
+                                    <li><a href="">Menu 2.2.2</a></li>
+                                    <li><a href="">Menu 2.2.3</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="">Menu 2.2</a>
+                                <ul class="sub-menu">
+                                    <li><a href="">Menu 2.2.1</a></li>
+                                    <li><a href="">Menu 2.2.2</a></li>
+                                    <li><a href="">Menu 2.2.3</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="">Menu 2.3</a>
+                                <ul class="sub-menu">
+                                    <li><a href="">Menu 2.2.1</a></li>
+                                    <li><a href="">Menu 2.2.2</a></li>
+                                    <li><a href="">Menu 2.2.3</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li><a href="">Hãng</a>
+                        <ul class="sub-menu">
+                            <li><a href="">NIKE</a></li>
+                            <li><a href="">ADIDAS</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="">Liên hệ</a></li>
+                </ul>
+
+                <div class="search-container">
+                    <input type="text" placeholder="Tìm kiếm...">
+                    <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
                 </div>
-            </div>
-            <div class="cart">
-                <a href="#"><i class="fas fa-shopping-cart"></i></a>
-                <!-- Link đến trang giỏ hàng sẽ được chuyển đến khi nhấp vào icon giỏ hàng -->
-            </div>
-        </div>
-    </nav>
 
-    <div>
-        <p>calnclasncaskn</p>
-    </div>
-    <!-- ---------------------------------------------------------------------------------------- -->
-    <div class="new-sp">
-        <a href="#"><h3>GIÀY NIKE</h3></a>
-        <div class="product">
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/17.Giày Nike Nam Chính Hãng - PEGASUS TURBO - Xám  JapanSport DM3413-005/nike-pegasus-turbo-dm3413-005-01-jpeg.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nam Chính Hãng PEGASUS TURBO JapanSport</h4>
-                <p class="product-price">Giá: 680.000đ</p> 
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/10.Giày Nike Nam Chính Hãng - Air Max SYSTM - Màu Đen  JapanSport DM9537-004/dm9537-004-1.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nam Chính Hãng Air Max SYSTM JapanSport </h4>
-                <p class="product-price">Giá: 720.000đ</p>
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/19.Giày Nike Nam Nữ Chính Hãng - Nike Dunk Mid - Đen  JapanSport FQ8784-100/dunk-mid-mens-shoes-qvrhs8-1-738837f9-0264-4a28-9382-01066189dcaa.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nam Nữ Chính Hãng Nike Dunk Mid</h4>
-                <p class="product-price">Giá: 510.000đ</p>
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/38.Giày Nike Nữ Chính Hãng - Air Max INTRLK - Hồng  JapanSport DQ2904-600/8a6e9d0b-accf-498a-b52f-dd6397b5397f.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nữ Chính Hãng Air Max INTRLK</h4>
-                <p class="product-price">Giá: 760.000đ</p>
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/50.Giày Nike Nam Chính Hãng - Air Jordan 1 Mid - ĐỏĐen  JapanSport DQ8426-106/nike-air-jordan-1-mid-dq8426-106-01.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nam Chính Hãng - Air Jordan</h4>
-                <p class="product-price">Giá: 640.000đ</p>
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/17.Giày Nike Nam Chính Hãng - PEGASUS TURBO - Xám  JapanSport DM3413-005/nike-pegasus-turbo-dm3413-005-01-jpeg.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nam Chính Hãng - PEGASUS TURBO</h4>
-                <p class="product-price">Giá: 820.000đ</p>
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/14.Giày Nike Nam Nữ Chính Hãng - Air Force 1 LV8 - Xanh  JapanSport FB9035-001/fb9035-001-01.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nam Nữ Chính Hãng Air Force</h4>
-                <p class="product-price">Giá: 630.000đ</p>
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            <div class="product-image">
-                <img src="../Image/GIÀY/NIKE/31.Giày Nike Nam Nữ Chính Hãng - (GS) Air Max Bolt White Pink - Trắng  JapanSport CW1626-200/cw1626-200-01.webp" alt="Product Image">
-                <h4 class="product-title">Giày Nike Nam Nữ Chính Hãng - (GS) Air Max Bolt</h4>
-                <p class="product-price">Giá: 870.000đ</p>
-                <button class="btn-buy">Mua Ngay</button>
-            </div>
-            
-        </div>
-
-        <!-- ------phân trang--------------- -->
-        <div class="pagination">
-            <a href="#" class="prev">&laquo;</a>
-            <a href="#" class="active">1</a>
-            <a href="nike2.php">2</a>
-
-            <a href="#" class="next">&raquo;</a>
-        </div> 
-    </div>       
-  
+                <div class="giohang-dangnhap-dangky">
+                    <a href="#"><button><i class="fa-solid fa-cart-shopping"></i></button></a>
+                    <a href="#"><button><i class="fa-solid fa-user"></i></button></a>
+                    <a href="login.php"><button class="logout">Đăng xuất</button></a>
+                </div>
+            </nav>
+        </header>
     
-
-    <!------------------------------------------footer------------------------------------------->
-
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <h4>Thông tin</h4>
-                    <ul>
-                        <li><a href="#">Về chúng tôi</a></li>
-                        <li><a href="#">Quy định</a></li>
-                        <li><a href="#">Chính sách bảo mật</a></li>
-                    </ul>
+        <!-- ---------------------------------------------------------------------------------------- -->
+        <div class="new-sp">
+            <a href="#"><h3>GIÀY NIKE</h3></a>
+            <div class="product">
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/17.Giày Nike Nam Chính Hãng - PEGASUS TURBO - Xám  JapanSport DM3413-005/nike-pegasus-turbo-dm3413-005-01-jpeg.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nam Chính Hãng PEGASUS TURBO JapanSport</h4>
+                    <p class="product-price">Giá: 680.000đ</p> 
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
                 </div>
-
-            
-                <div class="col-md-3">
-                    <h4>Hỗ trợ</h4>
-                    <ul>
-                        <li><a href="#">Trung tâm trợ giúp</a></li>
-                        <li><a href="#">Liên hệ</a></li>
-                    </ul>
-                </div> 
-
-                <div class="col-md-3">
-                    <h4>Theo dõi chúng tôi</h4>
-                    <ul>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Instagram</a></li>
-                        <li><a href="#">Twitter</a></li>
-                    </ul>
-                </div>        
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/10.Giày Nike Nam Chính Hãng - Air Max SYSTM - Màu Đen  JapanSport DM9537-004/dm9537-004-1.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nam Chính Hãng Air Max SYSTM JapanSport </h4>
+                    <p class="product-price">Giá: 720.000đ</p>
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
+                </div>
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/19.Giày Nike Nam Nữ Chính Hãng - Nike Dunk Mid - Đen  JapanSport FQ8784-100/dunk-mid-mens-shoes-qvrhs8-1-738837f9-0264-4a28-9382-01066189dcaa.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nam Nữ Chính Hãng Nike Dunk Mid</h4>
+                    <p class="product-price">Giá: 510.000đ</p>
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
+                </div>
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/38.Giày Nike Nữ Chính Hãng - Air Max INTRLK - Hồng  JapanSport DQ2904-600/8a6e9d0b-accf-498a-b52f-dd6397b5397f.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nữ Chính Hãng Air Max INTRLK</h4>
+                    <p class="product-price">Giá: 760.000đ</p>
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
+                </div>
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/50.Giày Nike Nam Chính Hãng - Air Jordan 1 Mid - ĐỏĐen  JapanSport DQ8426-106/nike-air-jordan-1-mid-dq8426-106-01.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nam Chính Hãng - Air Jordan</h4>
+                    <p class="product-price">Giá: 640.000đ</p>
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
+                </div>
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/17.Giày Nike Nam Chính Hãng - PEGASUS TURBO - Xám  JapanSport DM3413-005/nike-pegasus-turbo-dm3413-005-01-jpeg.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nam Chính Hãng - PEGASUS TURBO</h4>
+                    <p class="product-price">Giá: 820.000đ</p>
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
+                </div>
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/14.Giày Nike Nam Nữ Chính Hãng - Air Force 1 LV8 - Xanh  JapanSport FB9035-001/fb9035-001-01.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nam Nữ Chính Hãng Air Force</h4>
+                    <p class="product-price">Giá: 630.000đ</p>
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
+                </div>
+                <div class="product-image">
+                    <img src="../Image/GIÀY/NIKE/31.Giày Nike Nam Nữ Chính Hãng - (GS) Air Max Bolt White Pink - Trắng  JapanSport CW1626-200/cw1626-200-01.webp" alt="Product Image">
+                    <h4 class="product-title">Giày Nike Nam Nữ Chính Hãng - (GS) Air Max Bolt</h4>
+                    <p class="product-price">Giá: 870.000đ</p>
+                    <a href="#"><button class="btn-buy">Mua Ngay</button></a>
+                </div>
                 
-                <div class="col-md-3">
-                    <h4>Đăng ký nhận tin</h4>
-                    <p>Nhận thông tin mới nhất về sản phẩm và khuyến mãi.</p>
-                    <form action="#">
-                        <input type="email" placeholder="Nhập email của bạn">
-                        <button type="submit">Đăng ký</button>
-                    </form>
+            </div>
+    
+            <!-- ------phân trang--------------- -->
+            <div class="pagination">
+                <a href="#" class="prev">&laquo;</a>
+                <a href="#" class="active">1</a>
+                <a href="nike2.php">2</a>
+    
+                <a href="#" class="next">&raquo;</a>
+            </div> 
+        </div>       
+      
+        
+    
+        <!------------------------------------------footer------------------------------------------->
+    
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <h4>Thông tin</h4>
+                        <ul>
+                            <li><a href="#">Về chúng tôi</a></li>
+                            <li><a href="#">Quy định</a></li>
+                            <li><a href="#">Chính sách bảo mật</a></li>
+                        </ul>
+                    </div>
+        
+                  
+                    <div class="col-md-3">
+                        <h4>Hỗ trợ</h4>
+                        <ul>
+                            <li><a href="#">Trung tâm trợ giúp</a></li>
+                            <li><a href="#">Liên hệ</a></li>
+                        </ul>
+                    </div> 
+        
+                    <div class="col-md-3">
+                        <h4>Theo dõi chúng tôi</h4>
+                        <ul>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Twitter</a></li>
+                        </ul>
+                    </div>        
+                      
+                    <div class="col-md-3">
+                        <h4>Đăng ký nhận tin</h4>
+                        <p>Nhận thông tin mới nhất về sản phẩm và khuyến mãi.</p>
+                        <form action="#">
+                            <input type="email" placeholder="Nhập email của bạn">
+                            <button type="submit">Đăng ký</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
-    <!-- ----------nút cuộn về trang đầu------ -->
-    <button onclick="scrollToTop()" id="backToTopBtn" title="Go to top">
-        <i class="fas fa-chevron-up"></i>
-    </button>
+        </footer>
+        <!-- ----------nút cuộn về trang đầu------ -->
+        <button onclick="scrollToTop()" id="backToTopBtn" title="Go to top">
+            <i class="fas fa-chevron-up"></i>
+        </button>
+    </div>
     <script src="script.js"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/JS/trangchu.js"></script>
 </body>
 </html>
