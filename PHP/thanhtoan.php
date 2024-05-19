f527d5352d11c88f388d67cba2be010a4a9f0a69
Truong-2024 <160559362+Truong-2024@users.noreply.github.com>
2024-05-19 14:19:18 +0700

    Update thanhtoan.php

--- PHP/thanhtoan.php
+++ PHP/thanhtoan.php
@@ -1,127 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../CSS/thanhtoan.css">
-    <title>Thanh toán</title>
-</head>
-<body>
-    <div class="wapper">
-        <header>
-            <div class="logo">
-                <a href="#">
-                    <p class="web-name">TRUONGPHAT</p>
-                    <p class="page-name">Thanh Toán</p>
-                </a>
-            </div>
-        </header>
+<?php
+session_start();
+require "conn.php";
 
-        <div class="wp-content">
-            <div class="wp-content1 ">
-                <div class="diachinhanhang">
-                    <div class="name-diachinhanhang">
-                        <i class="fa-solid fa-location-dot"></i>
-                        <h2>Địa chỉ nhận hàng</h2>
-                    </div>
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['selected_products']) && is_array($_POST['selected_products'])) {
+    $selectedProducts = $_POST['selected_products'];
+    $paymentMethod = $_POST['payment_method'];
     
-                    <div class="wp-content-diachi">
-                        <div class="diachi">
-                            <div class="name-phone">Nguyễn Thành Phát (+84) 489316025</div>
-                            <div class="name-diachi">256 Đường Nguyễn Văn Cừ, phường An Hòa, quận Ninh Kiều, thành phố Cần Thơ</div>
-                        </div>
-                        <button class="change-address">Thay đổi</button>
-                    </div>
+    // Lưu thông tin đơn hàng vào cơ sở dữ liệu
+    $maDonHang = uniqid('DH'); // Tạo mã đơn hàng duy nhất
+    $tenKhachHang = isset($_SESSION['name_phone']) ? $_SESSION['name_phone'] : "Nguyễn Thành Phát";
+    $diaChi = isset($_SESSION['address']) ? $_SESSION['address'] : "256 Đường Nguyễn Văn Cừ, phường An Hòa, quận Ninh Kiều, thành phố Cần Thơ";
     
-                </div>
-            </div>
+    $tongTienHang = 0;
+    foreach ($selectedProducts as $maSanPham) {
+        foreach ($_SESSION['cart'] as $product) {
+            if ($product['MaSanPham'] == $maSanPham) {
+                $tongTienHang += $product['Gia'] * $product['SoLuong'];
+                // Lưu từng sản phẩm vào bảng chi tiết đơn hàng
+                $sqlChiTiet = "INSERT INTO ChiTietDonHang (MaDonHang, MaSanPham, SoLuong, Gia) VALUES ('$maDonHang', '{$product['MaSanPham']}', '{$product['SoLuong']}', '{$product['Gia']}')";
+                mysqli_query($conn, $sqlChiTiet);
+                break;
+            }
+        }
+    }
+
+    // Tính phí vận chuyển
+    $phiVanChuyen = 0;
+    if ($tongTienHang < 1000) {
+        $phiVanChuyen = 30;
+    } elseif ($tongTienHang < 5000) {
+        $phiVanChuyen = 50;
+    } elseif ($tongTienHang < 10000) {
+        $phiVanChuyen = 70;
+    } else {
+        $phiVanChuyen = 100;
+    }
     
-            <div class="wp-content2">
-                <div class="wp-content2-heading">
-                    <div class="header">
-                        <div class="header-content title">
-                            <h2 class="sanpham">Sản phẩm</h2>
-                        </div>
-
-                        <div class="header-content">Đơn giá</div>
-
-                        <div class="header-content">Số lượng</div>
-
-                        <div class="header-content thanhtien">Thành tiền</div>
-                    </div>
-                </div>
-
-                <div class="wp-content2-content">
-                    <div class="noidungsanpham">
-                        <div class="content-noidungsanpham">
-                            <div class="chitietsanpham content1-noidungsanpham">
-                                <img class="img" src="../Image/DÉP/1.Dép quai ngang - S cool EVA Slides/zen_0085_877eab6aea1547ffbe0a756229731d36_master.webp" alt="">
-                                <span class="title-img">Dép quai ngang - S cool EVA Slides</span>
-                            </div>
-
-                            <div class="chitietsanpham">290.000 <sup>đ</sup></div>
-
-                            <div class="chitietsanpham">1</div>
-
-                            <div class="chitietsanpham content2-noidungsanpham">290.000 <sup>đ</sup></div>
-                        </div>
-                    </div>
-
-                    <div class="underline"></div>
-
-                    <div class="thanhtien">
-                        <div class="content-thanhtien">
-                            <h3 class="title-content-thanhtien">Tổng số tiền:</h3>
-                            <div class="tien">290.000 <sup>đ</sup></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="wp-content3">
-                <div class="phuongthucthanhtoan">
-                    <div class="content-phuongthucthanhtoan"> 
-                        <div class="title-phuongthucthanhtoan"><p>Phương thức thanh toán</p></div>
-                        <div class="radio-phuongthucthanhtoan">
-                            <form class="radiogroup">
-                                <input class="inp" name="gender" type="radio" value="Nam"/><span>Thanh toán khi nhận hàng</span>
-                                <input class="inp" name="gender" type="radio" value="Nữ"/><span>Khác</span>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="cmt-phuongthucthanhtoan">
-                    <div class="content-cmt-phuongthucthanhtoan">
-                        <div class="content1-cmt-phuongthucthanhtoan">Thanh toán khi nhận hàng</div>
-                        <div class="content2-cmt-phuongthucthanhtoan">
-                            <div class="children-content2-cmt-phuongthucthanhtoan">
-                                Phí thu hộ: 0VNĐ. Ưu đãi về phí vận chuyển (nếu có) áp dụng cả với phí thu hộ. 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="dathang">
-                    <h3 class="content-dathang CT1 CM1">Tổng tiền hàng</h3>
-                    <div class="content-dathang CT2 CM1">290.000 <sup>đ</sup></div>
-                    <h3 class="content-dathang CT1 CM2">Phí vận chuyển</h3>
-                    <div class="content-dathang CT2 CM2">30.000 <sup>đ</sup></div>
-                    <h3 class="content-dathang CT1 CM3">Tổng thanh toán</h3>
-                    <div class="content-dathang tongtien CT2 CM3">320.000 <sup>đ</sup></div>
-                    <div class="last-dathang">
-                        <div class="content-last-dathang">
-                            <div class="mota-content-last-dathang">
-                                Nhấn "Đặt hàng" đồng nghĩa với việc bạn đồng ý với điều khoản của Shop
-                            </div>
-                        </div>
-                        <button class="btn-dathang">Đặt hàng</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</body>
-</html>
+    $tongThanhToan = $tongTienHang + $phiVanChuyen;
+    
+    // Lưu thông tin đơn hàng vào bảng đơn hàng
+    $sqlDonHang = "INSERT INTO DonHang (MaDonHang, TenKhachHang, DiaChi, TongTienHang, PhiVanChuyen, TongThanhToan, PhuongThucThanhToan) VALUES ('$maDonHang', '$tenKhachHang', '$diaChi', '$tongTienHang', '$phiVanChuyen', '$tongThanhToan', '$paymentMethod')";
+    mysqli_query($conn, $sqlDonHang);
+    
+    // Chuyển hướng đến trang hóa đơn
+    header("Location: hoadon.php?maDonHang=$maDonHang");
+    exit();
+} else {
+    echo "Có lỗi xảy ra. Vui lòng thử lại.";
+    exit();
+}
+?>
